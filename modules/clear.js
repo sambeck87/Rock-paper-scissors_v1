a16617baf96cf41a0c4fb271884ec94771def23a
sambeck87 <sandro_mipb@hotmail.com>
2023-07-28 15:35:47 -0600

    Implement the function that clear the move

---
+++ modules/clear.js
@@ -0,0 +1,17 @@
+export default function Clear() {
+  const divisor = document.getElementById('result');
+  const indication = document.getElementById('indication');
+  const space = document.getElementById('space');
+  setTimeout(() => {
+    const result = document.getElementById('winner');
+    result.remove();
+    divisor.classList.remove('result', 't-center')
+    divisor.classList.add('divisor')
+    indication.classList.remove('hide')
+    space.classList.add('hide')
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach(button => {
+      button.disabled = false;
+    });
+  }, 2000)
+}
